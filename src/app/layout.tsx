e941ed7066f899de9b792339c3e96dd0aa0d91ac
Tavinci <christertavi@gmail.com>
2026-02-11 04:08:17 +0200

    Add money bag emoji favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: "HowMuch Finance",
   description: "Personal finance tracker",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💰</text></svg>",
+  },
 }
 
 export default function RootLayout({
